83d5d6851ee13e08f9efe662765cf46455bb2312
ucigaș de capibara <catalinzavtone@gmail.com>
2026-05-14 09:46:46 +0300

    database introductio

---
+++ Python/database+python/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column,Integer,String,create_engine,inspect
+from sqlalchemy.orm import declarative_base
+
+db_url=("sqlite:///database.db")
+engine=create_engine(db_url)
+Base=declarative_base()
+
+
+class User(Base):
+    __tablename__='users'
+
+    id=Column(Integer,primary_key=True)
+    name=Column(String)
+    age=Column(Integer)
+
+Base.metadata.create_all(engine)
+
+inspector=inspect(engine)
+print(inspector.get_table_names())
--- Python/sideprojects/test.py
+++ Python/sideprojects/test.py
@@ -1,6 +1,4 @@
-def containsDuplicate(nums):
-    print(len(set(nums)))
-    print(set(nums))
-    print(len(nums))
-    return len(nums) != len(set(nums))
-print(containsDuplicate([1,1,1,3]))
+
+def climbStairs(n):
+    
+    return
