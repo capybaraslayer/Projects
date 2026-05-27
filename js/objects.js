888071b553848d3feeb39a9196f718309f75025c
ucigaș de capibara <catalinzavtone@gmail.com>
2026-05-27 20:31:35 +0300

    ceva nou la step

--- js/objects.js
+++ js/objects.js
@@ -90,8 +90,87 @@
 // person={
 
 // };
-const person={
-    name:'steven'
-};
+// const person={
+//     name:'steven'
+// };
 
-console.log(person.constructor);
+// console.log(person.constructor);
+// let newObj={}
+// let newObj=new Object();
+// new String();
+// new Boolean();
+// new Number();
+// function add(num1,num2){
+//     return num1+num2;
+// }
+// // const n =add;
+// // console.log(n(2,3));
+// console.log(add.length);
+
+// function Programer(name){
+//     this.name=name;
+//     this.writeCode=function(){
+//         console.log('Code in js')
+//     }
+// }
+
+// console.log(Programer.length);
+// const Programer=new Function('name',`
+//     this.name=name;
+//     this.writeCode=function(){
+//     console.log('Code in js')
+//      }`);
+
+// const newProgramer=new Programer('Steven');
+// newProgramer.writeCode();
+// console.log(newProgramer.constructor);
+
+// let a =10;
+// let b=a;
+
+// a=20;
+
+// console.log(a);
+// console.log(b);
+
+// let a ={value:20};
+// let b=a;
+
+// a.value=100;
+
+// console.log(a);
+// console.log(b);
+
+// let numbers=[1,2,3,4,5,6]
+// for (const element in numbers){
+//     console.log(element);
+// }
+
+const dog={
+    name:'Max',
+    age:4,
+    culoareOchilor:'albastrii'
+};
+// for(const key in dog){
+//     console.log(dog[key]);
+// }
+
+
+const keys=Object.keys(dog);
+// console.log(keys);
+const values=Object.values(dog);
+//console.log(values);
+const entrees =Object.entries(dog);
+// console.log(entrees);
+
+// for(const key of Object.keys(dog)){
+//     console.log(key);
+// }
+
+// for(const value of Object.values(dog)){
+//     console.log(value);
+// }
+
+for(const entry of Object.entries(dog)){
+    console.log(entry);
+}
