eab62c759991ba795e66a236fac6cb4193b7830a
ucigaș de capibara <catalinzavtone@gmail.com>
2026-05-29 22:16:54 +0300

    js

---
+++ .vscode/C++/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout<<"Enter your name"<<endl;
+    getline(cin,name);
+    cout<<"hello your name is:"<<name<<endl;
+
+    return 0;
+}
